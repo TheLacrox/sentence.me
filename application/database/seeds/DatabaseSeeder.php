5778d512a602d8f3aa51e31201dcd3d3b1c55362
TheLacrox <antonio.fran.sanchez.hernandez@gmail.com>
2019-06-05 23:10:06 +0100

    Añadido Seeders de Users

--- application/database/seeds/DatabaseSeeder.php
+++ application/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RoleSeeder::class);
         $this->call(PermissionSeeder::class);
+        $this->call(UsuariosSeeder::class);
     }
 }
---
+++ application/database/seeds/UsuariosSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Collection;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsuariosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Collection::make([
+            [
+                'name'=>'alumno1',
+                'email'=>'alumno1@gmail.com',
+                'password'=>Hash::make('adminadmin')
+            ],
+            [
+                'name'=>'alumno2',
+                'email'=>'alumno2@gmail.com',
+                'password'=>Hash::make('adminadmin')
+            ],
+        ])->each(function($item){
+            $user=User::updateOrCreate(
+                ['email'=>$item['email']],
+                $item
+            );
+            $user->assignRole('Alumno');
+        });
+        Collection::make([
+            [
+                'name'=>'profesor1',
+                'email'=>'profesor1@gmail.com',
+                'password'=>Hash::make('adminadmin')
+            ],
+            [
+                'name'=>'profesor2',
+                'email'=>'profesor2@gmail.com',
+                'password'=>Hash::make('adminadmin')
+
+            ],
+        ])->each(function($item){
+            $user=User::updateOrCreate(
+                ['email'=>$item['email']],
+                $item
+            );
+            $user->assignRole('Profesor');
+        });
+    }
+}
