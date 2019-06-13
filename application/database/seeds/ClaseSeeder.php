8cef55c469c008b5961c874f8b78b57a9382a631
Antonio Francisco Sanchez Hernandez <antonio.fran.sanchez.hernandez@gmail.com>
2019-06-13 11:24:50 +0000

    Apply fixes from StyleCI

--- application/database/seeds/ClaseSeeder.php
+++ application/database/seeds/ClaseSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Collection;
 use App\Clase;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Seeder;
 use Illuminate\Foundation\Auth\User;
 
 class ClaseSeeder extends Seeder
@@ -16,26 +16,25 @@ class ClaseSeeder extends Seeder
     {
         Collection::make([
             [
-                'nombre'=>'Clase Jose',
-                'descripcion'=>'Clase Sobre Java Para pruebas',
-                'clave'=>str_random('16')
-            ],[
-                'nombre'=>'Clase Alberto',
-                'descripcion'=>'Clase Sobre Php Para pruebas',
-                'clave'=>str_random('16')
-            ]
-        ])->each(function($item){
-            $clase =new Clase($item);
-            if($item["nombre"]=='Clase Jose'){
-                $id=3;
+                'nombre'     => 'Clase Jose',
+                'descripcion'=> 'Clase Sobre Java Para pruebas',
+                'clave'      => str_random('16'),
+            ], [
+                'nombre'     => 'Clase Alberto',
+                'descripcion'=> 'Clase Sobre Php Para pruebas',
+                'clave'      => str_random('16'),
+            ],
+        ])->each(function ($item) {
+            $clase = new Clase($item);
+            if ($item['nombre'] == 'Clase Jose') {
+                $id = 3;
                 $clase->user()->associate(User::find($id))->save();
                 $clase->Users()->save(User::find(1));
                 $clase->Users()->save(User::find(2));
-            }else{
-                $id=4;
+            } else {
+                $id = 4;
                 $clase->user()->associate(User::find($id))->save();
             }
-            ;
         });
     }
 }
